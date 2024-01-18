a05c4aa4bc442f5abf0cb1d3e0aaaf822b5bb31b
Alexandre MESSUVE <alexandre.messuves@gmail.com>
2024-01-18 13:54:26 +0100

    maj 8ball & fix de certaine commande

--- src/commands/pic.js
+++ src/commands/pic.js
@@ -1,29 +1,56 @@
+// Importing necessary modules
 import axios from "axios";
 import { SlashCommandBuilder, AttachmentBuilder } from "discord.js";
+import i18next from "i18next";
 import { Buffer } from 'node:buffer';
 
 
-const picCommande = {
+
+/**
+ * Represents a slash command for sending a random pic.
+ * @type {Object}picCommand
+ * @property {SlashCommandBuilder} data - The data property of the object defining the slash command.
+ * @property {Function} execute - The function that executes the command to retrieve and send a random image.
+ */
+const picCommand = {
+    // Defining the data property of the object
     data: new SlashCommandBuilder()
         .setName('pic')
         .setDescription('Send a random pic')
+        .setDescriptionLocalization('fr', 'Envoi une image aléatoire')
         .addStringOption(
             option => option.setName('category')
                 .setDescription('The pic category')
+                .setDescriptionLocalization('fr', 'Catégorie d\'image')
                 .setChoices(
-                    { name: 'Random category', value: 'random' },
+                    { name: 'Random category', value: 'random', name_localizations: { fr: 'Catégorie aléatoire' } },
                     { name: 'Nature', value: 'nature' },
-                    { name: 'City', value: 'city' },
-                    { name: 'Technology', value: 'technology' },
-                    { name: 'Food', value: 'food' },
-                    { name: 'Still Life', value: 'still_life' },
-                    { name: 'Wild Life', value: 'wild_life' },
-                    { name: 'Abstract', value: 'abstract' },
+                    { name: 'City', value: 'city', name_localizations: { fr: 'Ville' } },
+                    { name: 'Technology', value: 'technology', name_localizations: { fr: 'Technologie' } },
+                    { name: 'Food', value: 'food', name_localizations: { fr: 'Nourriture' } },
+                    { name: 'Still Life', value: 'still_life', name_localizations: { fr: 'Nature morte' } },
+                    { name: 'Wild Life', value: 'wildlife', name_localizations: { fr: 'Nature sauvage' } },
+                    { name: 'Abstract', value: 'abstract', name_localizations: { fr: 'Abstrait' } },
                 )
                 .setRequired(true)
+        )
+        .addStringOption(option =>
+            option.setName('lang')
+                .setDescription('Define the language')
+                .setDescriptionLocalization('fr', 'Défini le langage')
+                .addChoices(
+                    { name: 'English', value: 'en', name_localizations: { fr: 'Anglais' } },
+                    { name: 'French', value: 'fr', name_localizations: { fr: 'Français' } })
+                .setRequired(true)
         ),
+    /**
+     * Executes the command to retrieve and send a random image based on the specified category.
+     * @param {Interaction} interaction - The interaction object representing the command interaction.
+     * @returns {Promise<void>} - A promise that resolves once the command execution is complete.
+     */
     async execute(interaction) {
         const category = await interaction.options.getString('category') === 'random' ? '' : await interaction.options.getString('category');
+
         try {
             const resp = await axios.get(`https://api.api-ninjas.com/v1/randomimage?category=${category}`, {
                 headers: {
@@ -33,19 +60,16 @@ const picCommande = {
             });
             const buffer = new Buffer.from(resp.data, "base64");
             const picture = new AttachmentBuilder(buffer, { name: 'image.jpg' })
-               await interaction.reply({
-                content: 'Your pic here :',
+            await interaction.reply({
+                content: i18next.t('picMessage'),
                 files: [picture]
-               })
+            })
             console.log('Send pic successful');
         } catch (error) {
             console.error(error);
-            await interaction.reply('An error occured, please try later');
+            await interaction.reply(i18next.t('errorMessage'));
         }
-
-
-
     },
 };
 
-export default picCommande;
+export default picCommand;
